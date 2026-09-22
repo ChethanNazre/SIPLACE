259d7820e5c329585baf8313dd0f6b1787d184fd
ChethanNazre <chethannazre009@gmail.com>
2026-09-22 16:59:13 +0530

    Add SetupDetail model

---
+++ Models/SetupDetail.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace SiplaceApp.Models
+{
+    public class SetupDetail
+    {
+        public int DetailId { get; set; }
+        public int RecipeId { get; set; }
+        public string MachineName { get; set; }
+        public string Table { get; set; }
+        public string Track { get; set; }
+        public string PartNumber { get; set; }
+        public int Quantity { get; set; }
+        public string ReferenceDesignator { get; set; }
+        public string feederType { get; set; }
+        public virtual Recipe Recipe { get; set; }
+    }
+}
